a1c16233ef3fa6db6f123b82b57e6ad1f5d03133
root <vicentemanueleduardo@gmail.com>
2025-09-29 14:52:10 +0100

    fix: upload file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Complaint\ComplaintController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
+
+
+Route::get('/reports/show/{id}/file', [ComplaintController::class, 'showFile'])->name('reports.showFile');
