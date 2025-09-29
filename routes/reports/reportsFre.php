4e99f2ae8a7155300bf0c6b0722599d2e0e34fdf
root <vicentemanueleduardo@gmail.com>
2025-09-29 09:52:05 +0100

    refatory: modify isAnonymous

--- routes/reports/reportsFre.php
+++ routes/reports/reportsFre.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Complaint\ComplaintController;
+use App\Http\Controllers\Complaint\TypeComplaintsController;
 use App\Http\Controllers\Permission\PermissionController;
 use Illuminate\Support\Facades\Route;
 
