2d4f6988ecdbf19ad3118aac74aca30de0879f9f
root <vicentemanueleduardo@gmail.com>
2025-09-30 13:57:47 +0100

    fix: type report

--- app/Http/Controllers/Complaint/TypeComplaintsController.php
+++ app/Http/Controllers/Complaint/TypeComplaintsController.php
@@ -1,51 +1,72 @@
 <?php
-    
-    namespace App\Http\Controllers\Complaint;
-    
-    use App\Http\Controllers\AbstractController;
-    use App\Services\Complaint\TypeComplaintsService;
-    use App\Http\Requests\Complaint\TypeComplaintsRequest;
-    use Exception;
-    use Illuminate\Database\Eloquent\ModelNotFoundException;
-    use Illuminate\Http\Response;
-    
-    class TypeComplaintsController extends AbstractController
+
+namespace App\Http\Controllers\Complaint;
+
+use App\Http\Controllers\AbstractController;
+use App\Services\Complaint\TypeComplaintsService;
+use App\Http\Requests\Complaint\TypeComplaintsRequest;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+
+class TypeComplaintsController extends AbstractController
+{
+    public function __construct(TypeComplaintsService $service)
     {
-        public function __construct(TypeComplaintsService $service)
-        {
-            $this->service = $service;
-        }
-    
-        /**
-         * Store a newly created resource in storage.
-         */
-        public function store(TypeComplaintsRequest $request)
-        {
-            try {
+        $this->service = $service;
+    }
+
+    public function index(Request $request)
+    {
+        try {
+            if ($this->logRequest) {
                 $this->logRequest();
-                $typeComplaints = $this->service->store($request->validated());
-                return response()->json($typeComplaints, Response::HTTP_CREATED);
-            } catch (Exception $e) {
-                $this->logRequest($e);
-                return response()->json($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
             }
-        }
-    
-        /**
-         * Update the specified resource in storage.
-         */
-        public function update(TypeComplaintsRequest $request, $id)
-        {
-            try {
-                $this->logRequest();
-                $typeComplaints = $this->service->update($request->validated(), $id);
-                return response()->json($typeComplaints, Response::HTTP_OK);
-            } catch (ModelNotFoundException $e) {
+
+            $filters = $request['filters'] ?? $request['filtersV2'];
+
+            $service = $this->service->index(
+                $request['paginate'],
+                $filters,
+                $request->input('orderBy', ['id' => 'desc']),
+                $request->input('relationships', []),
+                $filters
+            );
+
+            return response()->json($service);
+        } catch (Exception $e) {
+            if ($this->logRequest) {
                 $this->logRequest($e);
-                return response()->json(['error' => 'Resource not found.'], Response::HTTP_NOT_FOUND);
-            } catch (Exception $e) {
-                $this->logRequest($e);
-                return response()->json($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
             }
+            return response()->json($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
+
+    public function store(TypeComplaintsRequest $request)
+    {
+        try {
+            $this->logRequest();
+            $typeComplaints = $this->service->store($request->validated());
+            return response()->json($typeComplaints, Response::HTTP_CREATED);
+        } catch (Exception $e) {
+            $this->logRequest($e);
+            return response()->json($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function update(TypeComplaintsRequest $request, $id)
+    {
+        try {
+            $this->logRequest();
+            $typeComplaints = $this->service->update($request->validated(), $id);
+            return response()->json($typeComplaints, Response::HTTP_OK);
+        } catch (ModelNotFoundException $e) {
+            $this->logRequest($e);
+            return response()->json(['error' => 'Resource not found.'], Response::HTTP_NOT_FOUND);
+        } catch (Exception $e) {
+            $this->logRequest($e);
+            return response()->json($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
--- routes/reports/reportsFre.php
+++ routes/reports/reportsFre.php
@@ -6,11 +6,5 @@ use App\Http\Controllers\Permission\PermissionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/getBycode/{id}', [ComplaintController::class, 'getBycode'])->name('reportsFre.php.index');
-Route::post('/', [ComplaintController::class, 'store'])->name('reportsFre.php.store');
-
 
 Route::get('/type', [TypeComplaintsController::class, 'index']);
-Route::post('/type', [TypeComplaintsController::class, 'store']);
-Route::get('/type/{id}', [TypeComplaintsController::class, 'show']);
-Route::put('/type/{id}', [TypeComplaintsController::class, 'update']);
-Route::delete('/type/{id}', [TypeComplaintsController::class, 'destroy']);
