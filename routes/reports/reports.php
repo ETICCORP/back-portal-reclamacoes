1abe51e139d55dd28235f71ad04db1ee08be0cfc
root <vicentemanueleduardo@gmail.com>
2025-10-01 16:14:21 +0100

    feat:denunias

--- routes/reports/reports.php
+++ routes/reports/reports.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\Complaint\TypeComplaintsController;
 use App\Http\Controllers\Permission\PermissionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ComplaintController::class, 'index'])->name('reports.index');
 
 Route::put('/{id}', [ComplaintController::class, 'update'])->name('reports.update');
 
--- routes/reports/reportsFre.php
+++ routes/reports/reportsFre.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Complaint\ComplaintController;
 use App\Http\Controllers\Complaint\TypeComplaintsController;
 use App\Http\Controllers\Permission\PermissionController;
 use Illuminate\Support\Facades\Route;
+Route::get('/', [ComplaintController::class, 'index'])->name('reports.index');
 
 Route::get('/getBycode/{id}', [ComplaintController::class, 'getBycode'])->name('reportsFre.php.index');
 
