64ab7a60052eadc6ba17823ec86ff27c55fc345b
root <vicentemanueleduardo@gmail.com>
2025-10-17 10:39:10 +0100

    feat: add logo global

--- routes/reports/reports.php
+++ routes/reports/reports.php
@@ -9,15 +9,12 @@ Route::get('/', [ComplaintController::class, 'index'])->name('reports.index');
 Route::put('/{id}', [ComplaintController::class, 'update'])->name('reports.update');
 Route::delete('/{id}', [ComplaintController::class, 'destroy'])->name('reports.destroy');
 
-
 Route::get('/total', [ComplaintController::class, 'total'])->name('complaints.total');
 Route::get('/total-current-week', [ComplaintController::class, 'totalForCurrentWeek'])->name('complaints.totalCurrentWeek');
 Route::get('/total-last-week', [ComplaintController::class, 'totalForLastWeek'])->name('complaints.totalLastWeek');
 Route::get('/types', [ComplaintController::class, 'getTopTypes'])->name('complaints.types');
 Route::get('/count-by-date', [ComplaintController::class, 'countByDate'])->name('complaints.countByDate');
 Route::post('/count-by-date', [ComplaintController::class, 'countByDate'])->name('countByDate.index');
-
-
 Route::put('/updateStatus/{id}', [ComplaintController::class, 'updateStatus']);
 
 
