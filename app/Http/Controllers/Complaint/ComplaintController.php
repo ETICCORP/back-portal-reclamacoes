b59b41dabf31bd715e8b3137b18d883a85137c41
Faustino-Etic <faustino.melo@etic.co.ao>
2025-10-28 12:10:12 +0100

    colocando o report como opcional

--- app/Http/Controllers/Complaint/ComplaintController.php
+++ app/Http/Controllers/Complaint/ComplaintController.php
@@ -90,13 +90,13 @@ class ComplaintController extends AbstractController
             
             AlertJob::dispatch($complaint->id); 
             
-            if($request->reporter['email']) {
+            if(!empty($request->reporter) || $request->reporter != null) {
                 Mail::to($request->reporter['email'])->send(new ReportAlertMail($request));
             }
 
             //SendReportCopy::dispatch($complaint->id);
             return response()->json($complaint, Response::HTTP_CREATED);
-            
+
         } catch (Exception $e) {
             $this->logRequest($e);
             return response()->json($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
