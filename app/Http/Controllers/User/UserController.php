916358bc2a1ca203f8e957d66f35d3021265f5ff
Carlos Bumba <carlos.calulo@etic.co.ao>
2026-05-07 16:25:27 +0100

    Update UserController.php

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -193,6 +193,11 @@ class UserController extends AbstractController
         try {
             $user = $this->service->verify2fa($request->validated());
             $this->logAction(params: $user);
+            $this->logToDatabase(
+                type: 'user',
+                level: 'info',
+                customMessage: 'Iniciou sessão na aplicação.',
+            );
             return response()->json($user, Response::HTTP_CREATED);
         } catch (Exception $e) {
             $this->logRequest($e);
