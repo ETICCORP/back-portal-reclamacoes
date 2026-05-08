f20de87ef8eead19930319ba7d1329d5cc6bf8c0
Carlos Bumba <carlos.calulo@etic.co.ao>
2026-05-08 17:44:03 +0100

    Update LogController.php

--- app/Http/Controllers/Log/LogController.php
+++ app/Http/Controllers/Log/LogController.php
@@ -4,13 +4,36 @@ namespace App\Http\Controllers\Log;
 
 use App\Services\Log\LogService;
 use App\Http\Controllers\AbstractController;
-
+use Illuminate\Http\Request;
+use Exception;
 
 class LogController extends AbstractController
 {
-    protected bool $logRequest = false;
+
     public function __construct(LogService $service)
     {
         $this->service = $service;
+    }
+
+    public function index(Request $request)
+    {
+        try {
+            // Captura de filtros
+            $filters = $request['filters'] ?? $request['filtersV2'];
+
+            // Chamada ao serviço (LogService)
+            $result = $this->service->index(
+                $request->input('paginate', 15),
+                $filters,
+                $request->input('orderBy', ['id' => 'desc']),
+                $request->input('relationships', []),
+            );
+
+            return response()->json($result);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 }
